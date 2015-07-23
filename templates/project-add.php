23752b370af144a933ceb96746c1e96693507e7c
Joseph Gabito <codehaiku.io@gmail.com>
2015-07-23 14:09:52 +0800

    New Update

--- templates/project-add.php
+++ templates/project-add.php
@@ -3,15 +3,21 @@
 	<form action="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>" method="post">
 
 		<input type="hidden" name="method"  value="thrive_transactions_update_project" />
+
 		<input type="hidden" name="action"  value="thrive_transactions_request" />
+		
 		<input type="hidden" name="no_json" value="yes" />
 
 		<div class="thrive-form-field">
 
 			<?php $placeholder = __( 'Enter the new title for this project', 'thrive' ); ?>
+			
 			<label for="thrive-project-name">
+
 				<?php esc_html_e( 'Project Name', 'thrive' ); ?>
+				
 			</label>
+			
 			<input required placeholder="<?php esc_attr_e( $placeholder ); ?>" type="text" name="title" id="thrive-project-name" />
 
 		</div>
@@ -19,7 +25,9 @@
 		<div class="thrive-form-field">
 
 			<label for="thrive-project-content">
+				
 				<?php esc_html_e( 'Project Details', 'thrive' ); ?>
+
 			</label>
 
 			<textarea id="thrive-project-content" name="content" rows="5" placeholder="<?php esc_html_e( 'Describe what this project is all about. You can edit this later.', 'thrive' );?>" required ></textarea>
@@ -72,25 +80,43 @@
 			</div>
 
 			<div class="thrive-form-field">
+
 				<div class="alignright">
+
 					<button id="thriveSaveProjectBtn" type="submit" class="button">
+
 						<?php esc_attr_e( 'Save Project', 'thrive' ); ?>
+
 					</button>
+
 				</div>
+
 				<div class="clearfix"></div>
+
 			</div>
 
 		<?php } else { ?>
+
 			<div id="message" class="error">
+
 				<?php esc_attr_e( 'Looks like you don\'t have any groups yet. Please join or create new group to start a project.?', 'thrive' ); ?>
+			
 			</div>
+			
 			<div class="thrive-form-field">
+
 				<div class="alignright">
+
 					<button type="button" disabled class="button danger">
+
 						<?php esc_attr_e( 'Save Project', 'thrive' ); ?>
+
 					</button>
+
 				</div>
+
 				<div class="clearfix"></div>
+
 			</div>
 		<?php } ?>
 		</form>	
