2f5d523c8c41c6003176caaa769c29dcd975d568
Joseph Gabito <codehaiku.io@gmail.com>
2015-09-29 18:55:52 +0800

    Wrapped Updater function to init hook

--- thrive-intranet.php
+++ thrive-intranet.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Thrive - Intranet
  * Description: A helper plugin for 'Thrive WordPress Theme'. Contains all the functions for project management, private pages access, etc.
- * Version: 1.4
+ * Version: 1.4.1
  * Author: Dunhakdis
  * Author URI: http://dunhakdis.me
  * Text Domain: thrive
@@ -71,24 +71,31 @@ function thrive_register_projects_component() {
 /**
  * Enable GitHub Updater Class
  */
-if ( is_admin() ) { 
+add_action( 'init', 'thrive_plugin_updater_init' );
 
-	include_once plugin_dir_path( __FILE__ ) . '/updater.php';
+function thrive_plugin_updater_init() {
 
-    $config = array(
-        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'thrive-intranet', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/codehaiku/Thrive-Intranet', // the GitHub API url of your GitHub repo
-        'raw_url' => 'https://raw.github.com/codehaiku/Thrive-Intranet/master', // the GitHub raw url of your GitHub repo
-        'github_url' => 'https://github.com/codehaiku/Thrive-Intranet', // the GitHub url of your GitHub repo
-        'zip_url' => 'https://github.com/codehaiku/Thrive-Intranet/zipball/master', // the zip url of the GitHub repo
-        'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-        'requires' => '4.0', // which version of WordPress does your plugin require?
-        'tested' => '4.0', // which version of WordPress is your plugin tested up to?
-        'readme' => 'README.md', // which file to use as the readme for the version number
-        'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
-    );
+	if ( is_admin() ) { 
 
-    new WP_GitHub_Updater( $config );
+		include_once plugin_dir_path( __FILE__ ) . '/updater.php';
+
+	    $config = array(
+	        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+	        'proper_folder_name' => 'thrive-intranet', // this is the name of the folder your plugin lives in
+	        'api_url' => 'https://api.github.com/codehaiku/Thrive-Intranet', // the GitHub API url of your GitHub repo
+	        'raw_url' => 'https://raw.github.com/codehaiku/Thrive-Intranet/master', // the GitHub raw url of your GitHub repo
+	        'github_url' => 'https://github.com/codehaiku/Thrive-Intranet', // the GitHub url of your GitHub repo
+	        'zip_url' => 'https://github.com/codehaiku/Thrive-Intranet/zipball/master', // the zip url of the GitHub repo
+	        'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+	        'requires' => '4.0', // which version of WordPress does your plugin require?
+	        'tested' => '4.0', // which version of WordPress is your plugin tested up to?
+	        'readme' => 'README.md', // which file to use as the readme for the version number
+	        'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+	    );
+
+    	new WP_GitHub_Updater( $config );
+
+	}
+	return;
 }
 ?>
