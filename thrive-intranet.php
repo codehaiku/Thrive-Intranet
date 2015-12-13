6f9717801d8a8f1bef543abb6865a3bf9e8032ee
Joseph Gabito <codehaiku.io@gmail.com>
2015-12-13 17:10:51 +0800

    Updated the version file

--- thrive-intranet.php
+++ thrive-intranet.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Thrive - Intranet
  * Description: A helper plugin for 'Thrive WordPress Theme'. Contains all the functions for project management, private pages access, etc.
- * Version: 1.5.1
+ * Version: 1.6.0
  * Author: Dunhakdis
  * Author URI: http://dunhakdis.me
  * Text Domain: thrive
